c86673910e84c72fa162555407e18b8c41c9db15
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-02 22:22:23 +0500

    add routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,15 +3,23 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $1 from "./routes/about.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/github/[username].tsx";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/search.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/about.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/github/[username].tsx": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/search.tsx": $6,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
